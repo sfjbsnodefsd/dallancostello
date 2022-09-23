06c34fcf7ca2ea9c0921b8a661cb7dba7218786b
sfjbssept <sfjbssept@gmail.com>
2022-09-23 15:44:14 +0000

    Updating rest api with MongoDB

--- NodeJS/Node-Rest-Api-Mongo/index.js
+++ NodeJS/Node-Rest-Api-Mongo/index.js
@@ -2,10 +2,11 @@ const express = require("express")
 const coursesRouter = require("./routes/courses")
 require("dotenv").config();
 const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
 
 const app = express();
-
-app.use(coursesRouter)
+app.use(bodyParser);
+app.use(coursesRouter);
 
 
 mongoose.connect(process.env.DB_CONNECTION_URL,() => {
