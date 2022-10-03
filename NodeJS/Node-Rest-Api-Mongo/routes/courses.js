d63630adcf4c5c56e2312d35b62a5713605e08be
sfjbssept <sfjbssept@gmail.com>
2022-10-03 12:01:34 +0000

    added put and delete

--- NodeJS/Node-Rest-Api-Mongo/routes/courses.js
+++ NodeJS/Node-Rest-Api-Mongo/routes/courses.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const ex= require("express")
 const Course = require("../models/course")
 const router = ex.Router();
@@ -12,9 +13,37 @@ router.get("/allcourses", async (req,res) => {
 });
 
 //add a course
- router.post("/",async(req,res) => {
+ router.post("/addcourse",async(req,res) => {
     try{
         Course.create(req.body);
+        res.json(course);
+    } catch (err) {
+        res.json(err)
+    }
+ })
+
+ //delete a course
+ router.delete("/delete/:courseID",async(req,res) => {
+    try{
+        await Course.remove({_id:req.params.courseID});
+        res.status(200).json({
+            message: "deleted successfully"
+        })
+    } catch (err) {
+        res.send(err)
+    }
+ })
+
+ //update course
+ //add a course
+ router.put("/update/:courseID",async(req,res) => {
+    const courseID = req.params.courseID;
+    try{
+        const course = await Course.updateOne(
+            {"_id": courseID},
+            req.body
+        );
+        res.json(course)
     } catch (err) {
         res.json(err)
     }
