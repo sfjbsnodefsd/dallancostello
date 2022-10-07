e464abc36a7e517f3b022072627dbe0be76b50d4
sfjbssept <sfjbssept@gmail.com>
2022-10-07 15:10:45 +0000

    buy, create implementation complete

--- NodeJS/e-commerce_microservice_application/product-service/index.js
+++ NodeJS/e-commerce_microservice_application/product-service/index.js
@@ -4,58 +4,68 @@ const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const amqp = require("amqplib");
-const Product= require('./Product');
-const isAuthenticated = require('../isAuthenticated');
+const Product = require("./Product");
+const isAuthenticated = require("../isAuthenticated");
 app.use(express.json());
 var channel, connection;
 
 mongoose.connect(
-    "mongodb://localhost:27017/product-service",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => {
-      console.log(`product service DB  Connected`);
-    }
-  );
-
-  async function connect(){
-    const amqpServer = "amqp://localhost:5672"
-    connection = await amqp.connect(amqpServer);
-    channel = await connection.createChannel();
-    await channel.assertQueue("PRODUCT")
-
+  "mongodb://localhost:27017/product-service",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log(`product service DB  Connected`);
   }
-  connect();
+);
 
-  //create a new product
-  //buy a new product
+async function connect() {
+  const amqpServer = "amqp://localhost:5672";
+  connection = await amqp.connect(amqpServer);
+  channel = await connection.createChannel();
+  await channel.assertQueue("PRODUCT");
+}
 
-  app.post("/product/create", isAuthenticated, async (req, res) => {
-    const {name, description, price} = req.body;
-    const newProduct = new Product({
-      name,
-      description,
-      price
-    });
-    return res.json(newProduct);
-  })
+connect();
+// create a new product
+// buy a new product
 
-  app.post("/product/buy", isAuthenticated, async (req, res) => {
-    const [ids] = req.body;
-    const products = await Product.find(_id, { $in: ids});
+app.post("/product/create", isAuthenticated, async (req, res) => {
+  const { name, description, price } = req.body;
+  const newProduct = new Product({
+    name,
+    description,
+    price,
+  });
+  newProduct.save();
+  return res.json(newProduct);
+});
 
-    channel.sendToQueue("ORDER", 
+// user will send a lift of the products that the user wants to buy , they will be identified by the product id
+// the order will be created of those products and the sum of the products prices will be the total billing amount
+
+app.post("/product/buy", isAuthenticated, async (req, res) => {
+  const { ids } = req.body;
+  const products = await Product.find({ _id: { $in: ids } });
+
+  channel.sendToQueue(
+    "ORDER",
     Buffer.from(
       JSON.stringify({
-      products,
-      userEmail: req.user.email,
-    })
+        products,
+        userEmail: req.user.email,
+      })
     )
-    );
-  });
+  );
+  channel.consume("PRODUCT", data => {
+    console.log("consuming product queue");
+     order = JSON.parse(data.content);
+     channel.ack(data);
+  })
+  return res.json(order)
+});
 
 app.listen(5001, () => {
-    console.log(`product service is working at port 5001`);
+  console.log(`product service is working at port 5001`);
 });
