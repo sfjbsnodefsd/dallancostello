4c9cd8a0445f11edc60ee76ed354b9eb0ed10162
Dallan Costello <dallan.costello@tcs.com>
2022-10-11 14:28:40 +0000

    implemented create pensioner method

--- NodeJS/project-pension-management/isAuthenticated.js
+++ NodeJS/project-pension-management/isAuthenticated.js
@@ -16,4 +16,4 @@ module.exports = async function isAuthenticated(req, res, next){
             next();
         }
     });
-};
+}
--- NodeJS/project-pension-management/pensioner-detail-service/index.js
+++ NodeJS/project-pension-management/pensioner-detail-service/index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 const amqp = require("amqplib");
-const pensioner = require("./Pensioner");
+const Pensioner = require("./Pensioner");
 const isAuthenticated = require("../isAuthenticated");
 app.use(express.json());
 var channel, connection;
 
 mongoose.connect(
-  "mongodb://localhost:27017/product-service",
+  "mongodb://localhost:27017/pensioner-detail-service",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   () => {
-    console.log(`product service DB  Connected`);
+    console.log(`pensioner detail service DB  Connected`);
   }
 );
 
@@ -24,47 +24,51 @@ async function connect() {
   const amqpServer = "amqp://localhost:5672";
   connection = await amqp.connect(amqpServer);
   channel = await connection.createChannel();
-  await channel.assertQueue("PRODUCT");
+  await channel.assertQueue("PENSIONER");
 }
 
 connect();
 // create a new product
 // buy a new product
 
-app.post("/product/create", isAuthenticated, async (req, res) => {
-  const { name, description, price } = req.body;
-  const newProduct = new Product({
+app.post("/pensioner/create", isAuthenticated, async (req, res) => {
+  const { name, dob, pan, salary, allowances, selfOrFamily, bankDetails} = req.body;
+  const newPensioner = new Pensioner({
     name,
-    description,
-    price,
+  dob,
+  pan,
+  salary,
+  allowances,
+  selfOrFamily,
+  bankDetails
   });
-  newProduct.save();
-  return res.json(newProduct);
+  newPensioner.save();
+  return res.json(newPensioner);
 });
 
 // user will send a lift of the products that the user wants to buy , they will be identified by the product id
 // the order will be created of those products and the sum of the products prices will be the total billing amount
 
-app.post("/product/buy", isAuthenticated, async (req, res) => {
-  const { ids } = req.body;
-  const products = await Product.find({ _id: { $in: ids } });
+// app.post("/product/buy", isAuthenticated, async (req, res) => {
+//   const { ids } = req.body;
+//   const products = await Product.find({ _id: { $in: ids } });
 
-  channel.sendToQueue(
-    "ORDER",
-    Buffer.from(
-      JSON.stringify({
-        products,
-        userEmail: req.user.email,
-      })
-    )
-  );
-  channel.consume("PRODUCT", data => {
-    console.log("consuming product queue");
-     order = JSON.parse(data.content);
-     channel.ack(data);
-  })
-  return res.json(order)
-});
+//   channel.sendToQueue(
+//     "ORDER",
+//     Buffer.from(
+//       JSON.stringify({
+//         products,
+//         userEmail: req.user.email,
+//       })
+//     )
+//   );
+//   channel.consume("PRODUCT", data => {
+//     console.log("consuming product queue");
+//      order = JSON.parse(data.content);
+//      channel.ack(data);
+//   })
+//   return res.json(order)
+// });
 
 app.listen(5001, () => {
   console.log(`product service is working at port 5001`);
