1e9f634582693c9065debaa66fb4e99da1f27e31
Dallan Costello <dallan.costello@tcs.com>
2022-10-26 15:13:41 +0000

    created first angular application.

---
+++ NodeJS/Rabbit-MQ-Microservices/consumer/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const amqp = require("amqplib");
+var channel, connection;
+async function connect(){
+    try{
+    const amqpServer = "amqp://localhost:5672"
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("Rabbit");
+    }
+    catch(err) {
+        console.log(err);
+    }
+
+};
+
+connect();
+
+
+app.get("/send", (req, res) => {});
+
+app.listen(5002, () =>{
+    console.log("server is running at port 5002");
+});
---
+++ NodeJS/Rabbit-MQ-Microservices/producer/index.js
@@ -0,0 +1,26 @@
+//const { connect } = require("amqplib");
+const express = require("express");
+const app = express();
+const amqp = require("amqplib");
+var channel, connection;
+async function connect(){
+    try{
+    const amqpServer = "amqp://localhost:5672"
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("Rabbit");
+    }
+    catch(err) {
+        console.log(err);
+    }
+
+};
+
+connect();
+
+
+app.get("/send", (req, res) => {});
+
+app.listen(5000, () =>{
+    console.log("server is running at port 5000");
+});
