763aaf704a612e19c41811b307c49e613bffff42
Dallan Costello <dallan.costello@tcs.com>
2022-10-27 15:40:29 +0000

    added routing and services to Angular app.

---
+++ Angular/HelloWorldApp/src/app/Components/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+const routes: Routes = [{path:"", component:HomeComponent}, {path:"register", component:UserFormComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- Angular/HelloWorldApp/src/app/app.module.ts
+++ Angular/HelloWorldApp/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { AppRoutingModule } from './Components/app-routing.module';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './Components/user-form/user-form.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
+    UserFormComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
