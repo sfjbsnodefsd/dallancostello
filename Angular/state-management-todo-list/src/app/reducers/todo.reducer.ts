5b243080e526d454115b6f26ce27531012f66e10
Dallan Costello <dallan.costello@tcs.com>
2022-11-01 15:30:36 +0000

    Created todo angular app.

---
+++ Angular/state-management-todo-list/src/app/reducers/todo.reducer.ts
@@ -0,0 +1,12 @@
+import { ActionParent } from "../actions/todo.actions";
+import { Todo } from "../Entity/Todo";
+
+const initialState: Todo[]= [
+{title: "Dummy title 1"}
+]
+
+export function TodoReducer(state =initialState, action:ActionParent) {
+    switch(action.type) {
+        default: return state;
+    }
+}
